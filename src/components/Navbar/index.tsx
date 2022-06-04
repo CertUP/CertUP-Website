52538fff002b1df722d194ec3f5d642c184b827e
Xiphiar <me@xiphiar.com>
2022-06-04 13:50:38 -0400

    use png logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,12 +3,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import Button from '../CUButton';
 import KeplrButton from '../KeplrButton';
 
 import styles from './styles.module.scss';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/certup-logo.svg';
+//import logo from '../../assets/certup-logo.svg';
+import logo from '../../assets/certup-logo-small.png';
 
 export default function CertUpNavbar() {
   return (
@@ -16,12 +18,14 @@ export default function CertUpNavbar() {
       <Container>
         <Link to="/">
           <Navbar.Brand>
-            <img
+            <Image
               src={logo}
-              width="158"
-              height="62"
+              fluid={true}
+              // width="158"
+              // height="62"
+              style={{ height: '80px' }}
               className="d-inline-block align-top"
-              alt="React Bootstrap logo"
+              alt="CertUP Logo"
             />
           </Navbar.Brand>
         </Link>
